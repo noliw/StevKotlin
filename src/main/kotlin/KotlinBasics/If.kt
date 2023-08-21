2745c14fdd037cfd309b4171f13653ecf5d9adad
noliw <noliw2017@gmail.com>
2023-08-21 01:32:43 -0700

    If

---
+++ src/main/kotlin/KotlinBasics/If.kt
@@ -0,0 +1,20 @@
+package KotlinBasics
+
+fun main() {
+    ageRestriction(age = 18)
+}
+
+fun ageRestriction(age: Int){
+    if (age < 18) println("Access Denied!")
+    if (age >= 18) println("Access Approved!")
+}
+
+fun checkNumSign(number: Int): String {
+    return if (number > 0){
+        "positive"
+    } else if (number < 0){
+        "Negative"
+    } else {
+        "Zero"
+    }
+}
