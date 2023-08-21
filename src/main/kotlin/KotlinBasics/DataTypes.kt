98b0ad3a92620c117f331a6be195d6f5595854a6
noliw <noliw2017@gmail.com>
2023-08-21 01:04:31 -0700

    Data types complete

--- src/main/kotlin/KotlinBasics/DataTypes.kt
+++ src/main/kotlin/KotlinBasics/DataTypes.kt
@@ -1,2 +1,16 @@
 package KotlinBasics
 
+fun main() {
+    // byte, short, int, long
+    // float, double
+    val number = 5
+    val doubleNumber = 4.99 // can have upto 15 nums after the decimal point
+    val floatNUmber = 4.99f  // floats can only have 6-7 numbers after the decimal points
+    val letter = 'A'
+    val boolean = true
+    val text = "This is a string"
+
+
+    val example = 1 + 1.5
+    val example2 = "Number: " + 1.5
+}
