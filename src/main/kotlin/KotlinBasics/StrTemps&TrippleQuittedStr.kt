65f8bffea03a08842ef8d9ad6baaf8ba4fb8e44d
noliw <noliw2017@gmail.com>
2023-08-21 01:44:38 -0700

    string template

---
+++ src/main/kotlin/KotlinBasics/StrTemps&TrippleQuittedStr.kt
@@ -0,0 +1,20 @@
+package KotlinBasics
+
+fun main() {
+    // A string template is a programatic way of generating a string
+    val number = 10
+    val favNum = "My favorite number is : ${number.toFloat()}"
+    println(favNum)
+
+    val about = "dsrfhvskj kjsdvhsdhklv bhb\n " +
+            "hbh bhfbdjljvebv jbhjelrehjbdshv bhfdjlfvbh\n " +
+            "h bljfkdslfs lkfjrkfj jkjk "
+
+    val about1= """skflhervfkb jkfdbvkfj 
+        |dfjkvbfkdjvelfjvjf,f fojvvbdjk fdjkvhdjk 
+        |jfkdhfvieuvfbd kdfj dfjkdjk""".trimMargin()
+}
+
+fun setAge(age: Int) {
+    println(if (age < 18) "Minor" else "Adult")
+}
