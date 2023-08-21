47159123499dfc78ca27696eaa1c41cf99bd0886
noliw <noliw2017@gmail.com>
2023-08-21 02:14:56 -0700

    For loops

---
+++ src/main/kotlin/KotlinBasics/ForLoops&Ranges.kt
@@ -0,0 +1,19 @@
+package KotlinBasics
+
+fun main() {
+    val element = listOf("A", "B")
+    for (e in element){
+        println("Current Element: $e")
+    }
+
+    for (number in 1..3){
+        println("Current number: $number")
+    }
+
+    var range1 = 1..10
+    var step = 1..10 step 2
+    var range2 = 1 until 10
+    var range3 = 10 downTo  0
+    var chars = 'a'..'z'
+
+}
---
+++ src/main/kotlin/KotlinBasics/WhileLoop.kt
@@ -0,0 +1,15 @@
+package KotlinBasics
+
+fun main() {
+    var chars = printEachLetter("NOlawi")
+    println(chars)
+}
+
+
+fun printEachLetter(string: String) {
+    var i = 0
+    while (i < string.length){
+        println(string[i])
+        i++
+    }
+}
