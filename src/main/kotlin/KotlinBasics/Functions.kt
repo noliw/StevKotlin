ab99592ff4899e13d662d12293849cee290d6439
noliw <noliw2017@gmail.com>
2023-08-21 01:21:20 -0700

    functions

---
+++ src/main/kotlin/KotlinBasics/Functions.kt
@@ -0,0 +1,23 @@
+package KotlinBasics
+
+
+// functions are a great way to organize code by splitting it into smaller pieces
+fun main() {
+    sumOfTwoNumbs()
+    val result = sumOfTwoNumbs(3, 7)
+    println(result)
+}
+
+fun sumOfTwoNumbs() {
+    println(2 + 2)
+}
+
+
+// This is a block body function define with the curly braces
+fun sumOfTwoNumbs(a: Int, b: Int): Int {
+    return a + b
+}
+
+
+// this is expression body
+fun expBdy (a: Int, b: Int) = a * b
