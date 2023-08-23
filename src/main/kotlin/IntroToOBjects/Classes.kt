d1a372a2f9ad4ea81aeb7429fa05d46e0ba402fa
noliw <noliw2017@gmail.com>
2023-08-23 01:12:17 -0700

    Classes

---
+++ src/main/kotlin/IntroToOBjects/Classes.kt
@@ -0,0 +1,21 @@
+package IntroToOBjects
+
+// objects are used to store data using properties and perform operations with that data using functions
+
+// classes are blueprints
+
+fun main() {
+    val house = House() // this variable represents an object of the house class
+    val house2 = House()
+
+    println(house) // IntroToOBjects.House@7b23ec81 this is the address where the object is located
+    println(house2) // IntroToOBjects.House@6acbcfc0 notice how it is stored in different locations
+
+    println(house.enter())
+
+
+}
+
+class House () {
+    fun enter() = "Entering the house.."  // this is called a member function because it belongs to that class specifically
+}
