cee2ffd5039fcf77aaa3c2285580d08f3d903285
noliw <noliw2017@gmail.com>
2023-08-23 01:36:43 -0700

    Constructor

--- src/main/kotlin/02-IntroToOBjects/03-Constructor.kt
+++ src/main/kotlin/02-IntroToOBjects/03-Constructor.kt
@@ -1,2 +1,24 @@
 package `02-IntroToOBjects`
 
+// a constructor is used to construct something. A constroctor is a special member function
+// that initializes a new object
+
+/*
+they are used to pass some information like we used with function parameters
+ */
+fun main() {
+    val person = People(name = "Noli")
+    println(person.toString())
+}
+
+
+class People (val name: String = "UNKNOWN") {
+    fun getName(){
+        println(name)
+    }
+
+    override fun toString(): String {
+        return name
+    }
+}
+
